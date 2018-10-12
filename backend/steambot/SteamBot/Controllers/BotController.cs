24786e536eac58037d68d90229a5ad2336e5c25e
Deivydas Voroneckis <v.deivydas2@gmail.com>
2018-10-13 01:35:54 +0300

    Added steam bot with method to send message to all friends.

---
+++ backend/steambot/SteamBot/Controllers/BotController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SteamBot.Services;
+
+namespace SteamBot.Controllers
+{
+    [Route("api/bot")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post(string messageText)
+        {
+            var service = new MessageService(messageText);
+            var isSent = service.SendMessage();
+            return Ok(isSent);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
